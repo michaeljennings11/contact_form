6294422f62a015a70ca4e9c3f205a9c8f8ce2a97
Michael Jennings <robertmjenningsjr@gmail.com>
2025-09-29 17:38:32 -0700

    removed junk commented lines

--- send-email.php
+++ send-email.php
@@ -36,18 +36,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = $mailHost;                     //Set the SMTP server to send through (e.g. 'smtp.gmail.com)
-        //$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through (e.g. 'smtp.gmail.com)
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = $mailUsername;                     //SMTP username
-        //$mail->Username   = 'user@example.com';                     //SMTP username
         $mail->Password   = $mailPassword;                               //SMTP password
-        //$mail->Password   = 'secret';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
         $mail->setFrom($mailFromAddress, $mailFromName);
-        //$mail->setFrom('from@example.com', 'Mailer');
         $mail->addAddress($mailRecipientAddress, $mailRecipientName);     //Add a recipient
         //$mail->addAddress('ellen@example.com');               //Name is optional
         $mail->addReplyTo($email, $name);
